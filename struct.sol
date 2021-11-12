125eb1deacf045054341ccfcf720b6043fcee20d
veridelisi <72710290+veridelisi@users.noreply.github.com>
2021-11-12 16:56:55 +0300

    Create struct.sol

---
+++ struct.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+contract Contract1 {
+
+    struct MyStruct {
+        string name;
+        uint age;
+        bool isMan;
+    }
+
+
+    MyStruct struct1 = MyStruct("Engin",40,true);
+    MyStruct struct2 = MyStruct("Filiz",40,false);
+    MyStruct struct3= MyStruct("Emre",35,true);
+
+ 
+
+    function getNameFromStruct() public view returns (string memory ) {
+        return struct1.name;
+    }
+
+    function getNameFromStruct2() public view returns (string memory) {
+        return struct2.name;
+    }
+
+    function demoStruct4() public pure returns (string memory) {
+      MyStruct memory struct4 = MyStruct("Su",4,false);
+       
+        return struct4.name;
+        
+    }
+    
+
+}
