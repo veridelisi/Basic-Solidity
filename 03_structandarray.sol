bd4ad7bfe30a2567251327b405711e8441558dad
veridelisi <72710290+veridelisi@users.noreply.github.com>
2021-11-16 10:33:32 +0300

    Update 03_structandarray.sol

--- 03_structandarray.sol
+++ 03_structandarray.sol
@@ -3,29 +3,25 @@
 pragma solidity ^0.6.0;
 
 contract Contract1{
-    
-    // this will get initialized to 0!
-    uint256 public favoriteNumber;
-   
+ 
     
     struct People {
         uint256 favoriteNumber;
         string name;
     }
     
+    
+    
     People[] public people;
+    
+    //Struct'ı array haline getirdik.
    
-    
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-    }
-    
-    function retrieve() public view returns(uint256) {
-        return favoriteNumber;
-    }
+
     
     function addPerson(string memory _name, uint256 _favoriteNumber) public{
         people.push(People(_favoriteNumber, _name));
+        
+        //Array'e push metoduyla ekleme yapıyoruz
         
     }    
     
