f8cce9c63e4b40a25f15e57c61277b742a813096
veridelisi <72710290+veridelisi@users.noreply.github.com>
2021-12-08 15:20:29 +0300

    Update 08_01_array.sol

--- 08_01_array.sol
+++ 08_01_array.sol
@@ -5,24 +5,34 @@ contract Array {
 
 uint[] public seri;
 
-
+//Add elements
 function set(uint i) public {
 
     seri.push(i);
 }
 
-//Tüm seri
+//Get all array
 function get() public view returns (uint[] memory){
 
     return seri;
 }
 
-//Serinin istenilen sıradaki elemanı
+//Get spesific element
 function get2(uint i) public view returns (uint){
 
     return seri[i];
 }
 
+//Remove the last element
+function pop() public {
+    
+        seri.pop();
+    }
+    
+//Lenght of the array
+function getLength() public view returns (uint) {
+        return seri.length;
+    }
 
 
 }
