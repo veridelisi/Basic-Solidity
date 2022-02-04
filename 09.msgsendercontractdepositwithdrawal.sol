9d609d22a5d3c8cb5c3b7742ce9df395cf1b46f6
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-02-05 00:59:15 +0300

    Create 09.msgsendercontractdepositwithdrawal.sol

---
+++ 09.msgsendercontractdepositwithdrawal.sol
@@ -0,0 +1,61 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "hardhat/console.sol";
+
+contract Bank {
+
+  mapping( address => uint256 ) balances;
+
+ 
+
+  constructor() public {}
+
+  function deposit() public payable {
+      balances[msg.sender] -= msg.value;
+      address(this).balance += msg.value;
+  }
+
+  function see() public view returns (address){
+        return msg.sender;
+    }
+
+   function getBalance() public view returns(uint256){
+        return msg.sender.balance;
+    }
+
+    function getBalance2() public view returns(uint256){
+        return address(this).balance;
+    }
+
+
+  function withdraw_via_transfer(uint256 amount) public {
+      // forwards 2300 gas, not adjustable
+      require(balances[msg.sender] >= amount, "Invalid withdraw request");
+      payable(msg.sender).transfer(amount);
+       balances[msg.sender] += amount;
+       
+  }
+
+  function withdraw_via_send(uint256 amount) public {
+      // forwards 2300 gas, not adjustable
+      // returns success condition
+      // fails if stack depth is at 1024
+      require(balances[msg.sender] >= amount, "Invalid withdraw request");
+      bool sent = payable(msg.sender).send(amount);
+      require(sent, "Failed to send Ether");
+       balances[msg.sender] += amount;
+     
+  }
+
+  function withdraw_via_call(uint256 amount) public {
+      // forwards all available gas
+      // returns success condition and data
+      require(balances[msg.sender] >= amount, "Invalid withdraw request");
+      // (bool sent, bytes memory _data) = msg.sender.call.value(amount}("");
+      (bool sent, bytes memory _data) = msg.sender.call{value: amount}("");
+      require(sent, "Failed to send Ether");
+      balances[msg.sender] += amount;
+     
+  }
+}
