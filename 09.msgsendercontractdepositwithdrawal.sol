bb3ccdffac63e184814719649cac21ceb9656bee
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-02-07 12:01:39 +0300

    Update 09.msgsendercontractdepositwithdrawal.sol

--- 09.msgsendercontractdepositwithdrawal.sol
+++ 09.msgsendercontractdepositwithdrawal.sol
@@ -6,10 +6,11 @@ pragma solidity ^0.8.11;
 contract Bank {
 
   mapping( address => uint256 ) balances;
+     
 
   function deposit() public payable {
-      balances[msg.sender] += msg.value;
-   
+      balances[msg.sender] += msg.value;     
+      
   }
 
   function see() public view returns (address){
@@ -27,7 +28,7 @@ contract Bank {
 
   function withdraw_via_transfer(uint256 amount) public {
       // forwards 2300 gas, not adjustable
-      require(balances[msg.sender] >= amount, "Invalid withdraw request");
+      require(address(this).balance >= amount, "Invalid withdraw request");
       payable(msg.sender).transfer(amount);
        balances[msg.sender] += amount;
        
@@ -37,7 +38,7 @@ contract Bank {
       // forwards 2300 gas, not adjustable
       // returns success condition
       // fails if stack depth is at 1024
-      require(balances[msg.sender] >= amount, "Invalid withdraw request");
+      require(address(this).balance >= amount, "Invalid withdraw request");
       bool sent = payable(msg.sender).send(amount);
       require(sent, "Failed to send Ether");
        balances[msg.sender] += amount;
@@ -47,7 +48,7 @@ contract Bank {
   function withdraw_via_call(uint256 amount) public {
       // forwards all available gas
       // returns success condition and data
-      require(balances[msg.sender] >= amount, "Invalid withdraw request");
+      require(address(this).balance >= amount, "Invalid withdraw request");
       // (bool sent, bytes memory _data) = msg.sender.call.value(amount}("");
       (bool sent, bytes memory _data) = msg.sender.call{value: amount}("");
       require(sent, "Failed to send Ether");
