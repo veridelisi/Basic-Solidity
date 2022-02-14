32a49ec832ee47e9387c0ba2581015dcdcdaf5ee
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-02-14 15:35:24 +0300

    Create virtual_override_1.sol

---
+++ virtual_override/virtual_override_1.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: MIT
+	pragma solidity ^0.8.0;
+	
+
+	 contract A {
+	     function foo()  public pure returns (string memory) {
+	         return "Foo from A";
+	     }
+	
+
+	     function bor()  public pure returns (string memory) {
+	         return "Bor from A";
+	     }
+
+	 }
+	
+
+	 contract B is A {
+	     function foo2()  public pure returns (string memory) {
+	         return foo();
+	     }
+	
+
+	     function bor2()  public pure returns (string memory) {
+	         return "Bor from B";
+	     }
+
+	    
+	 }
