f2bee9e81567341c7ca45d1287049f595d5c2005
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-02-26 22:07:12 +0300

    Create TestToken.sol

---
+++ erc20/TestToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.5;
+// Thanks to Deniz
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20
+{
+    constructor(string memory name, string memory symbol) public ERC20(name,symbol)
+    {
+        _mint(msg.sender, 1000000);
+    }
+}
