7e0e7b0fc3209fd91a6b85b4ebbf36b9ddda4ed2
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-02-28 16:25:23 +0300

    Update TestToken.sol

--- erc20/TestToken.sol
+++ erc20/TestToken.sol
@@ -1,12 +1,14 @@
-pragma solidity ^0.8.5;
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.11;
 // Thanks to Deniz
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20
 {
-    constructor(string memory name, string memory symbol) public ERC20(name,symbol)
+    constructor(string memory name, string memory symbol)  ERC20(name,symbol)
     {
-        _mint(msg.sender, 1000000);
+        _mint(msg.sender, 1000);
     }
 }
