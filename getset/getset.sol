96f054f5c7d5b8a6dce9c5c5c857c633f2a8939c
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-03-17 15:57:57 +0300

    Create getset.sol

---
+++ getset/getset.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+contract SimpleStorage{
+    uint256 public favoriteNumber;
+    
+   
+    
+    function set(uint256 _favoriteNumber) public {
+        favoriteNumber=_favoriteNumber;
+        
+
+    }
+    
+    function get() public view returns(uint256) {
+        return favoriteNumber;
+        
+
+    }
+    
+    
+}
