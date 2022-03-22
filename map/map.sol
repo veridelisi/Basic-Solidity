456003408bd23d5e3a197d572fc85f84edce66ad
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-03-22 16:55:10 +0300

    Update map.sol

--- map/map.sol
+++ map/map.sol
@@ -3,49 +3,38 @@
 
 pragma solidity ^0.8.3;
 
-contract Mapping {
+contract NestedMapping {
+    // Nested mapping (mapping from address to another mapping)
+    mapping(address => mapping(uint => uint)) public dolartl;
 
-    // Mapping from address to uint
-    mapping(address => uint) public myMap;
-    
-    function set(address _addr, uint _i) public {
-        // Update the value at this address
-        myMap[_addr] = _i;
+
+    function set(address _addr1,uint _dolar, uint _tl) public {
+       dolartl[_addr1][_dolar] = _tl;
     }
 
-    function get(address _addr) public view returns (uint) {
-        // Mapping always returns a value.
-        // If the value was never set, it will return the default value.
-        return myMap[_addr];
-    }
-
-    function remove(address _addr) public {
-        // Reset the value to the default value.
-        delete myMap[_addr];
+    function remove(address _addr1, uint _dolar) public {
+        delete dolartl[_addr1][_dolar];
     }
 }
 
-contract NestedMapping {
+contract Mapping {
+    mapping(address => bool) public registered;
+    mapping(address => int256) public favNums;
 
-    // Nested mapping (mapping from address to another mapping)
-    mapping(address => mapping(uint => bool)) public nested;
-    
-    function set(
-        address _addr1,
-        uint _i,
-        bool _boo
-    ) public {
-        nested[_addr1][_i] = _boo;
+    function register(int256 _favNum) public {
+       
+        require(!isRegistered(), "User also is registered");
+        registered[msg.sender] = true;
+        favNums[msg.sender] = _favNum;
     }
 
-    function get(address _addr1, uint _i) public view returns (bool) {
-        // You can get values from a nested mapping
-        // even when it is not initialized
-        return nested[_addr1][_i];
+    function isRegistered() public view returns (bool) {
+        return registered[msg.sender];
     }
 
-
-    function remove(address _addr1, uint _i) public {
-        delete nested[_addr1][_i];
+    function deleteRegistered() public {
+        require(isRegistered(), "Please register.");
+        delete (registered[msg.sender]);
+        delete (favNums[msg.sender]);
     }
 }
