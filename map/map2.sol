f33e9cd98524b4ff24a72fbc44572442135a9662
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-03-31 11:51:37 +0300

    Update map2.sol

--- map/map2.sol
+++ map/map2.sol
@@ -4,27 +4,20 @@ pragma solidity ^0.8.11;
 contract students {
 
 //from class address to students' number
-mapping(address => uint) public class;
-
-// from student's name to his age
-mapping(string => uint ) public student;
-
-
-// from student's name and his class number to his exam success position
-mapping(string => mapping(uint => uint)) public studentposition;
+mapping(address => uint) public classMap;
 
 //add class address and students' number
-function addClass(address classAddress, uint studentsnumber) public {
-         class[classAddress]=studentsnumber;
+function addClass(address class, uint studentsnumber) public {
+         classMap[class]=studentsnumber;
 }
 
-//add student's name and his age
-function addStudent(string memory name, uint age )public {
-        student[name]=age;
-}
+// from student's name and his class number to student exam note
+mapping(string => mapping(uint => uint)) public examNote;
+
+
 // add student's name and his class number and his exam success position
-function addStudentPosition( string memory studentname, uint studentnumber, uint position) public {
-    studentposition[studentname][studentnumber]=position;
+function addStudentPosition( string memory studentname, uint studentnumber, uint note) public {
+    examNote[studentname][studentnumber]=note;
 }
 
 }
