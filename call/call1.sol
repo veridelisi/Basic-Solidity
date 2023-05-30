402e7ec575add78ad60a9ce4dbe123f857988577
veridelisi <72710290+veridelisi@users.noreply.github.com>
2023-05-30 10:59:50 +0300

    Create call1.sol

---
+++ call/call1.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract CrossContract {
+  
+
+    function getLowerPrice(
+        address _priceOracle1,
+        address _priceOracle2
+    ) external view returns (uint256) {
+    
+      //Call function
+      if(PriceOracle1(_priceOracle1).price() < PriceOracle2(_priceOracle2).price()){
+            return PriceOracle1(_priceOracle1).price();
+        }
+        else {
+         return PriceOracle2(_priceOracle2).price();
+        }
+        
+         /** Call variable
+      if(PriceOracle1(_priceOracle1)._price() < PriceOracle2(_priceOracle2)._price()){
+            return PriceOracle1(_priceOracle1)._price();
+        }
+        else {
+         return PriceOracle2(_priceOracle2)._price();
+        }
+        */
+        
+    }
+}
+
+contract PriceOracle1 {
+    uint256 public _price;
+
+    function setPrice(uint256 newPrice) public {
+        _price = newPrice;
+    }
+
+    function price() external view returns (uint256) {
+        return _price;
+    }
+}
+
+contract PriceOracle2 {
+    uint256 public _price;
+
+    function setPrice(uint256 newPrice) public {
+        _price = newPrice;
+    }
+
+    function price() external view returns (uint256) {
+        return _price;
+    }
+}
